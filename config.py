9441287dc394f71f1da7d2bc45f3217b4ec2214c
eggoisticboy <141562776+eggoisticboy@users.noreply.github.com>
2023-08-11 11:12:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001903508531"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "─‌ᴍᴏᴏɴʟɪɢʜᴛ ᴍᴜsɪᴄ")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5665115127 ").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5920701493 , 5886326864").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
