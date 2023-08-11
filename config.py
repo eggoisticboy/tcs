91aac428aab79ae24104b89271742034d731600f
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-08-11 10:59:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,23 +7,23 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "23737933"))
-API_HASH = getenv("API_HASH", "de913d82c5e0cfdcdc08313c49304ae3")
+API_ID = int(getenv("API_ID", None))
+API_HASH = getenv("API_HASH", None)
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6139173699:AAEDPkAEOSMhUjg1QcrQER-Cu4X7u2Qxv4g")
+BOT_TOKEN = getenv("BOT_TOKEN", None)
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://vibadef654:vibadef654@cluster0.xb6gmqo.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001969560344"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "─‌⃜𓆩ᴛ¢s 𝙓 𝐌υѕι¢𓆪ꭗ‌")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5665115127 5655799578").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5665115127 ").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Raichuop07/tcs")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv("GIT_TOKEN", "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")
+GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_tcs")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/tcs_chatting_hub_forever")
@@ -45,8 +45,8 @@ PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "5"))
 TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "3"))
 
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "5363619fbddd46bb80021085e46c620e")
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "5d3180c8869847cebc15dab2a2586b90")
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 VIDEO_STREAM_LIMIT = int(getenv("VIDEO_STREAM_LIMIT", "5"))
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "50"))
@@ -58,8 +58,8 @@ TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "104857600"))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824"))
 # https://www.gbmb.org/mb-to-bytes
 
-STRING1 = getenv("STRING_SESSION", "BQBBLOcq-iopRcxa8fW7NZMXktmJIVCQrHMlJpYIZEjuvMgMpzIX0HYCqSSdvQtMzp134EEoDHMOYEcFE8QK8WEbihvZXuwYsLCBhuRtCS7fyniGotP4soH1YyrtNHYd3yeMFbnwtTCsDHFonCjVp9s447OX4fTeyGCklZTF8Hit8iVFKWNKLF9cYsQj7_rN04bknXZLQr9doTYIt0_lejZ8-mjAkXIBXoWNHyHYD4geNEcq9Zcy6yJXcPmSDTBLp74slX709WCJm15NCGy1RaMDc2K98yt1QOL-o5fNt838fUCcTrLSbZkulKbw2kW86iJxBVOGDbSBtbZZaunR_kSWAAAAAXInMzsA")
-STRING2 = getenv("STRING_SESSION2", "BQBiu2eohqCXhvof55thF9AmFxbxEuS5QP0DVGmCSxEazumqvvTYrzfYnwUrgO8g6wG3dju4BOx3CVAHMjwKmp243-8YwRNhOZm_Lk5yeYK7ihTE2IfO5L29uGDItNO2fZ156JKTTvaqSqu7fwuLikK9dW8GTcZgOURAFe-xAZbuUbwCkSAijpWB4EGMgcyvldxaFCeNnG_-9gJqBr2J7F3q-8lPCGhY7tybZcOhLXVHl3o8PD365TM9FA4R2XKS2JHMuxAli0CW-QyJB1D4u24EZORQk8_gHVpEepaGDDrgYAUFpLP4COiA53SCUl0bIbvOo3CbrEZrHv_kZk1icfnsAAAAAXvVDmkA")
+STRING1 = getenv("STRING_SESSION", None)
+STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
