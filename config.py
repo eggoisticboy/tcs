b36871ca3ad36af43ca6241a6ac05a25f7412ed8
eggoisticboy <141562776+eggoisticboy@users.noreply.github.com>
2023-08-11 11:14:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5920701493 , 5886326864").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Raichuop07/tcs")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/eggoisticboy/tcs")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
